7277bf56d4bbd4b39b9916829f9ec33d5ec327c8
Altaf <62064062+altaf1996@users.noreply.github.com>
2021-07-07 16:26:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
+
+
+# Home Page
 @app.route("/")
 @app.route("/home")
 def index():
     return render_template("index.html") 
 
+# About Page
 @app.route("/about", methods=["POST", "GET"])
 def about():
     return render_template("about.html")
 
+# More Page
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     return render_template("contact.html")
+
+# Cart Pagr
+@app.route("/cart", methods=["POST", "GET"])
+def cart():
+    return render_template("cart.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
